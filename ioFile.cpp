65c47ef0fca696b80bf33b98b9939ba61c7b73d1
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-06-10 10:07:21 +0700

    membuat code untuk membuka file dan menunjuk ke sebuah file dan ada displaynya

--- ioFile.cpp
+++ ioFile.cpp
@@ -6,5 +6,12 @@ using namespace std;
 int main(){
     string baris;
 
+    //mmebuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">=Menulis file, \'q\' untuk keluar" << endl;
+
 
 }
