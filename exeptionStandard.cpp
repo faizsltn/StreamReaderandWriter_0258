cf0d1ea5cb1a8b6a6c6e25a934844b02a8b185ae
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-06-10 11:01:26 +0700

    membuat int main dan try

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -5,3 +5,14 @@
 //untuk obyek array yang akan kita gunakan
 using namespace std;
 
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:
+    try{
+        array<int, 3> data = {10, 20, 30};
+        //int data[3] = {10, 20, 30}
+        //pesan array interger 3 elemen
+        cout << data.at(5) << endl;
+        //memanggil array elemen ke 5
+    }
+}
