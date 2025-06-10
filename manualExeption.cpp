89fe23f63b6cf9eb2abc9482aeb7b757f4fe6b67
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-06-10 10:55:24 +0700

    membuat program utama serta ada try,catch(int),catch (...)

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    try{
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 5; //melemparkan sebuah interger maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a){
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    catch(...){
+        /*jika selain interger maka block ini akan di eksekusi*/
+        cout << "default Pengecualian diexekusi" << endl;
+    }
+
+    return 0;
+    
+}
