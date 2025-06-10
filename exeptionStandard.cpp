8d591f3a28e3c9c6ffad22089536274f86b4dc64
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-06-10 11:05:28 +0700

    membuat catch dan display untuk penanda baris terakhir/ massage

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -15,4 +15,13 @@ int main()
         cout << data.at(5) << endl;
         //memanggil array elemen ke 5
     }
+    catch (exception& e){
+        //penangkap menggunakan obyek exception 
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanya memiliki 3 element */ 
+    }
+    cout << "Baris program yang terakhir" << endl;
+    /*Penanda 2 : bahwa program berjalan tanpa henti meskipun terjadi kesalahan */
+    return 0;
+
 }
